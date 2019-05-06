1ddfb0cebb54d1187f94421bb4bb35ddb0ec1b39
jennabianco <47002286+jennabianco@users.noreply.github.com>
2019-05-06 02:05:44 -0400

    Add files via upload

---
+++ collection/dresses/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  console.log("ready");
+  $(".box1").click(function() {
+    $(".text1").toggleClass("show");
+  });
+  
+});
+
+
+$(document).ready(function() {
+  console.log("ready");
+  $(".box2").click(function() {
+    $(".text2").toggleClass("show");
+  });
+  
+});
+
+$(document).ready(function() {
+  console.log("ready");
+  $(".box3").click(function() {
+    $(".text3").toggleClass("show");
+  });
+  
+});
+
+$(document).ready(function() {
+  console.log("ready");
+  $(".box4").click(function() {
+    $(".text4").toggleClass("show");
+  });
+  
+});
+
+$(document).ready(function() {
+  console.log("ready");
+  $(".box5").click(function() {
+    $(".text5").toggleClass("show");
+  });
+  
+});
---
+++ collection/headpiece/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  console.log("ready");
+  $(".box1").click(function() {
+    $(".text1").toggleClass("show");
+  });
+  
+});
+
+
+$(document).ready(function() {
+  console.log("ready");
+  $(".box2").click(function() {
+    $(".text2").toggleClass("show");
+  });
+  
+});
+
+$(document).ready(function() {
+  console.log("ready");
+  $(".box3").click(function() {
+    $(".text3").toggleClass("show");
+  });
+  
+});
