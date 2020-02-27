214bb549310fad2324d457884191a81fb3b39f45
jennabianco <47002286+jennabianco@users.noreply.github.com>
2020-02-26 20:29:33 -0500

    Add files via upload

---
+++ alarmclock/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".alarmsun").addClass("alarm");
+    $(".alarmcity").addClass("alarm");
+    $(".alarmwords").addClass("alarm");
+
+  }, 3000);
+});
